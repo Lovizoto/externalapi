e61c508f4e460ebe45ac6018a7b1eb3c616a6937
Computador PC <miche@NB-GERAL1.INFO.CORP>
2025-08-08 14:24:51 -0300

    update and devlopment

--- src/main/java/br/com/lovizoto/externalapi/cache/config/RedisConfig.java
+++ src/main/java/br/com/lovizoto/externalapi/cache/config/RedisConfig.java
@@ -8,18 +8,31 @@ import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableCaching
 public class RedisConfig {
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        Map<String, RedisCacheConfiguration> cacheConfigurations = new HashMap<>();
+
+        // TTL for "users" cache
+        cacheConfigurations.put("users", RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(30)));
+
+        // TTL for "sessions" cache
+        cacheConfigurations.put("sessions", RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofHours(2)));
+
         return RedisCacheManager.builder(redisConnectionFactory)
                 .cacheDefaults(
                         RedisCacheConfiguration.defaultCacheConfig()
-                                .entryTtl(Duration.ofHours(1)) // life time of the cache
+                                .entryTtl(Duration.ofHours(1)) // default TTL
                                 .disableCachingNullValues()
                 )
+                .withInitialCacheConfigurations(cacheConfigurations) // personal customs
                 .build();
     }
 }
